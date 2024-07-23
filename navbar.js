705f7d013ad0eb425ae0f6cb20139f969ad2d9fb
Yesui Ulziibayar <yulzii789@gmail.com>
2024-07-23 01:16:43 -0500

    small styling updates

--- navbar.js
+++ navbar.js
@@ -1,12 +1,27 @@
-// fixed navbar
-const navbar=document.getElementById("navbar");
-const content=document.getElementById("home");
+const navbar = document.getElementById("navbar");
 
-const scrollThreshold=60;
-window.addEventListener("scroll", () =>{
-    if(window.scrollY > scrollThreshold) {
+const scrollThreshold = 60;
+window.addEventListener("scroll", () => {
+    if (window.scrollY > scrollThreshold) {
         navbar.classList.add("fixed");
-    } else{
+    } else {
         navbar.classList.remove("fixed");
+    }
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const scrollArrowBtn = document.getElementById("scrollArrowBtn");
+    const experienceSection = document.getElementById("experience");
+
+    if (scrollArrowBtn) {
+        window.addEventListener("scroll", () => {
+            const scrollTop = window.scrollY;
+            const maxScroll = 200;
+            let opacity = 1 - Math.min(scrollTop / maxScroll, 1);
+            scrollArrowBtn.style.opacity = opacity;
+        });
+        window.addEventListener("click", () => {
+            experienceSection.scrollIntoView({ behavior: "smooth" });
+        });
     }
 });
