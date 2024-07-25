40208a56d1295b6c9e33092e3be559bea5abda06
Yesui Ulziibayar <yulzii789@gmail.com>
2024-07-25 14:48:18 -0500

    made the experience section more interactive

--- navbar.js
+++ navbar.js
@@ -29,10 +29,24 @@ document.addEventListener("DOMContentLoaded", () => {
         
     }
     
+    //change background color when row is hovered over
+    document.querySelectorAll('.styled-table tr').forEach(row => {
+    row.addEventListener('mouseover', () => {
+        const shaded = row.querySelectorAll('.shaded').forEach(cell => {
+            cell.classList.add('hovered');
+        });
+    });
+    row.addEventListener('mouseout', () => {
+        row.querySelectorAll('.shaded').forEach(cell => {
+            cell.classList.remove('hovered');
+        });
+    });
+});
+
+
 });
 
 //resume pdf display when resume button is clicked
-
 const resumeBtn = document.getElementById("resumeBtn");
 resumeBtn.addEventListener("click", () => {
     window.open("resume 2024.pdf", "_blank");
